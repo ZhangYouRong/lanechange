e769c88b6a6c7acdf16f6090d2419ddba411c791
zhangyr <zyr8@foxmail.com>
2020-05-03 22:27:14 +0800

    2020.5.3 version

--- main.py
+++ main.py
@@ -81,6 +81,8 @@ def plot_data(data):
     # ax6 = plt.subplot(2, 2, 4)
     # ax6.set_title('车辆实际运动路径图')
     plt.plot(delta_x, delta_y, color='m')
+    data = np.load('D:\software\CARLA_0.9.5\workspace\lanechange\exp2020-04-22-10-33-48\ddpg.npy')
+    plt.plot(data)
     plt.xlabel('x (m)', fontdict)
     plt.ylabel('y (m)', fontdict)
     plt.show()
